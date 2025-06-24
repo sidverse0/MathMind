f7ec451a9bdc0598979d04a6dca878f115008fe1
Arnold Bonit <arnoldbonit@gmail.com>
2025-06-24 19:25:58 +0000

    When touch on avtar in showing the top of dashboard. The open profile pa

--- src/components/user-nav.tsx
+++ src/components/user-nav.tsx
@@ -2,57 +2,18 @@
 
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
 import { Button } from "@/components/ui/button"
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuGroup,
-  DropdownMenuItem,
-  DropdownMenuLabel,
-  DropdownMenuSeparator,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu"
-import { useRouter } from "next/navigation";
-import { LogOut, User, Settings } from "lucide-react";
+import Link from 'next/link'
 
 export function UserNav() {
-  const router = useRouter();
-
   return (
-    <DropdownMenu>
-      <DropdownMenuTrigger asChild>
-        <Button variant="ghost" className="relative h-8 w-8 rounded-full">
-          <Avatar className="h-8 w-8">
-            <AvatarImage src="https://files.catbox.moe/uvi8l9.png" alt="@user" />
-            <AvatarFallback>U</AvatarFallback>
-          </Avatar>
-        </Button>
-      </DropdownMenuTrigger>
-      <DropdownMenuContent className="w-56" align="end" forceMount>
-        <DropdownMenuLabel className="font-normal">
-          <div className="flex flex-col space-y-1">
-            <p className="text-sm font-medium leading-none">User</p>
-            <p className="text-xs leading-none text-muted-foreground">
-              user@example.com
-            </p>
-          </div>
-        </DropdownMenuLabel>
-        <DropdownMenuSeparator />
-        <DropdownMenuGroup>
-          <DropdownMenuItem onSelect={() => router.push('/app/profile')}>
-            <User className="mr-2 h-4 w-4" />
-            <span>Profile</span>
-          </DropdownMenuItem>
-          <DropdownMenuItem>
-            <Settings className="mr-2 h-4 w-4" />
-            <span>Settings</span>
-          </DropdownMenuItem>
-        </DropdownMenuGroup>
-        <DropdownMenuSeparator />
-        <DropdownMenuItem>
-            <LogOut className="mr-2 h-4 w-4" />
-          <span>Log out</span>
-        </DropdownMenuItem>
-      </DropdownMenuContent>
-    </DropdownMenu>
+    <Button asChild variant="ghost" className="relative h-8 w-8 rounded-full">
+      <Link href="/app/profile">
+        <Avatar className="h-8 w-8">
+          <AvatarImage src="https://files.catbox.moe/uvi8l9.png" alt="@user" />
+          <AvatarFallback>U</AvatarFallback>
+        </Avatar>
+        <span className="sr-only">User Profile</span>
+      </Link>
+    </Button>
   )
 }
