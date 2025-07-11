44f87ad1fe411fd863a51ee211930c200de45035
REYAZXSID <reyazmbi2003@gmail.com>
2025-07-11 22:39:57 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -166,14 +166,14 @@ export default function LandingPage() {
                   </div>
               </Link>
               <p className="text-sm text-muted-foreground">
-                  Built by{' '}
+                  Credit | {' '}
                   <a 
                   href="https://portfolioxsid.vercel.app/"
                   target="_blank" 
                   rel="noopener noreferrer" 
                   className="font-semibold bg-clip-text text-transparent bg-gradient-to-r from-primary via-blue-500 to-teal-500 hover:opacity-80 transition-opacity inline-flex items-center gap-1 group"
                   >
-                  Reyaz
+                  SidVerse.
                   <Heart className="h-4 w-4 text-primary transition-transform group-hover:scale-125 group-hover:fill-primary" />
                   </a>
               </p>
